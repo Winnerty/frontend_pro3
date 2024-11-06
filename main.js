1d57124f28a00e616c1cbe62a600a00daeef7199
Winnerty <timofey.n.karpeev@gmail.com>
2024-11-06 09:55:16 +0100

    fddff

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { fetchUsers } from "./utils/fetchData"
-
 import "./style.css"
-
 import { handleData } from "./utils/handleData"
+import { modalComp } from "./components/modalComp"
 
 const usersArray = await fetchUsers()
 if (usersArray.length) handleData(usersArray)
+app.appendChild(modalComp())
